08f4590be76c0b1dead98e318ee0eb1e2162d590
Hoang Anh <doviethoanganh07@gmail.com>
2025-05-25 20:40:43 +0700

    Update anime.py

--- bot/anime.py
+++ bot/anime.py
@@ -1,25 +1,18 @@
-import requests
+import random
 
 def register_anime(bot):
     @bot.message_handler(commands=['anime'])
     def handle_anime(message):
         try:
-            # Lấy video URL từ API
-            api_url = "https://api-anime-0rz7.onrender.com/api/anime"
-            response = requests.get(api_url, timeout=40).json()
-            video_url = response['video_url']
+            file_path = "bot/url/anime.txt"
+            with open(file_path, "r", encoding="utf-8") as file:
+                video_urls = [line.strip() for line in file if line.strip()]
             
-            try:
-                # Gửi video lên Telegram
-                bot.send_video(
-                    chat_id=message.chat.id,
-                    video=video_url,
-                    reply_to_message_id=message.message_id,
-                    timeout=20
-                )
-            except Exception as e:
-                # Xử lý lỗi khi gửi video (ví dụ: URL lỗi)
-                bot.reply_to(message, f"Link lỗi: {video_url}")
+            if not video_urls:
+                bot.reply_to(message, "Danh sách video chưa có dữ liệu!")
+                return
+
+            selected_video = random.choice(video_urls)
+            bot.send_video(chat_id=message.chat.id, video=selected_video, reply_to_message_id=message.message_id)
         except Exception as e:
-            # Xử lý lỗi khi gọi API
-            bot.reply_to(message, "Lỗi API!")
+            bot.reply_to(message, f"Lỗi: {e}")
