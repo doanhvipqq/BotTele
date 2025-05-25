e3ee0ed3ab7d827f5f3a067bcc1e9341bb3aaf96
Hoang Anh <doviethoanganh07@gmail.com>
2025-05-25 13:10:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 # main_bot.py
 import os
 import telebot
+from bot.nct import register_nct
+from bot.scl import register_scl
 from bot.girl import register_girl
 from bot.images import register_images
-from bot.scl import register_scl
 
 # Lấy token từ biến môi trường
 TOKEN = "7757320016:AAEyc-YORyiR2aPz4UTrz7LHNHveSq9NgZw"
 bot = telebot.TeleBot(TOKEN)
 
 # Đăng ký các handler từ các file module khác nhau
+register_nct(bot)
+register_scl(bot)
 register_girl(bot)
 register_images(bot)
-register_scl(bot)
 
 if __name__ == '__main__':
     print("Bot đang chạy...")
