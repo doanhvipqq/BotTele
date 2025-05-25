27d10b415878ae20dc84cb9f3e16860fc200a3c6
Hoang Anh <doviethoanganh07@gmail.com>
2025-05-25 12:21:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
-from telegram.ext import Updater
-from config import BOT_TOKEN
+# main_bot.py
+import os
+import telebot
+from girl import register_girl
+from images import register_images
+from scl import register_scl
 
-# Import handler từ các module con
-from handlers.girl_handler import get_handler as girl_handler
-from handlers.images_handler import get_handler as images_handler
-from handlers.sourceweb_handler import get_handler as sourceweb_handler
-from handlers.tiktok_handler import get_handler as tiktok_handler
-from handlers.anime_handler import get_handler as anime_handler
-from handlers.emoji_handler import get_handler as emoji_handler
-from handlers.scl_beta_handler import get_handler as scl_beta_handler
+# Lấy token từ biến môi trường
+TOKEN = "7757320016:AAEyc-YORyiR2aPz4UTrz7LHNHveSq9NgZw"
+bot = telebot.TeleBot(TOKEN)
 
-def main():
-    updater = Updater(BOT_TOKEN, use_context=True)
-    dp = updater.dispatcher
-
-    # Thêm các handler vào dispatcher
-    dp.add_handler(girl_handler())
-    dp.add_handler(images_handler())
-    dp.add_handler(sourceweb_handler())
-    dp.add_handler(tiktok_handler())
-    dp.add_handler(anime_handler())
-    dp.add_handler(emoji_handler())
-    dp.add_handler(scl_beta_handler())
-
-    updater.start_polling()
-    updater.idle()
+# Đăng ký các handler từ các file module khác nhau
+register_girl(bot)
+register_images(bot)
+register_scl(bot)
 
 if __name__ == '__main__':
-    main()
+    print("Bot đang chạy...")
+    bot.infinity_polling()
