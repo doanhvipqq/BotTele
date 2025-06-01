0a052bffac7e5ef2651eb2ad7863c5fda1c4269f
Hoang Anh <doviethoanganh07@gmail.com>
2025-06-01 17:45:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import os
 import telebot
 from bot.nct import register_nct
 from bot.scl import register_scl
-from bot.girl import register_girl
-from bot.anime import register_anime
+from bot.ramdom import *
 from bot.images import register_images
 
 # Lấy token từ biến môi trường
@@ -14,8 +13,18 @@ bot = telebot.TeleBot(TOKEN)
 # Đăng ký các handler từ các file module khác nhau
 register_nct(bot)
 register_scl(bot)
+
+# Random
 register_girl(bot)
 register_anime(bot)
+register_imganime(bot)
+register_butt(bot)
+register_breastsqueeze(bot)
+register_cosplay(bot)
+register_pussy(bot)
+register_nude(bot)
+register_girlsexy(bot)
+
 register_images(bot)
 
 if __name__ == '__main__':
