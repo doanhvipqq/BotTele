25374375e67a3b035c047427062fb69799015727
Hoang Anh <doviethoanganh07@gmail.com>
2025-06-06 20:37:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from bot.nct import register_nct
 from bot.scl import register_scl
 from bot.random import *
 from bot.images import register_images
+from bot.github import register_github
 
 # Lấy token từ biến môi trường
 TOKEN = os.getenv("TELEGRAM_TOKEN")
@@ -24,8 +25,9 @@ register_cosplay(bot)
 register_pussy(bot)
 register_nude(bot)
 register_girlsexy(bot)
-
+#############################
 register_images(bot)
+register_github(bot)
 
 if __name__ == '__main__':
     print("Bot đang chạy...")
