4b23c06fe79d10e4410a31323c40e71ae282545d
Hoang Anh <doviethoanganh07@gmail.com>
2025-06-06 21:13:27 +0700

    Create time.py

---
+++ bot/time.py
@@ -0,0 +1,39 @@
+import time
+import random
+from datetime import timedelta
+
+start_time = time.time()
+file_path = "bot/url/anime"
+
+def get_uptime():
+    return str(timedelta(seconds=int(time.time() - start_time)))
+
+def anime_video():
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            video_urls = [line.strip() for line in file if line.strip()]
+        
+        if not video_urls:
+            return None
+
+        return random.choice(video_urls)
+    except Exception as e:
+        return None
+
+def register_time(bot):
+    @bot.message_handler(commands=['time'])
+    def send_time(message):
+        uptime = get_uptime()
+        video_url = anime_video()
+
+        if not video_url:
+            bot.reply_to(message, "Không thể lấy video anime.")
+            return
+        
+        caption = f"» <b>Bot đã hoạt động được:</b> [{uptime}]"
+        bot.send_photo(
+            message.chat.id,
+            video_url,
+            caption = caption,
+            parse_mode='HTML'
+        )
