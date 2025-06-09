75ee74b0fd50f56e3e49b134e3c96ef63fa23ec3
Hoang Anh <doviethoanganh07@gmail.com>
2025-06-09 17:29:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bot.scl import register_scl
 from bot.time import register_time
 from bot.help import register_help
 from bot.random import *
-from bot.getzip import register_getzip
+from bot.lxmanga import register_lxmanga
 from bot.tiktok import register_tiktok
 from bot.images import register_images
 from bot.github import register_github
@@ -21,7 +21,7 @@ register_nct(bot)
 register_scl(bot)
 register_time(bot)
 register_help(bot)
-register_getzip(bot)
+register_lxmanga(bot)
 register_tiktok(bot)
 register_search(bot)
 register_thongtin(bot)
