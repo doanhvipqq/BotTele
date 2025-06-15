6d6b336bed9518dfd521f7e583782e4dfb5a64b1
Hoang Anh <doviethoanganh07@gmail.com>
2025-06-15 23:42:38 +0700

    Update random.py

--- bot/random.py
+++ bot/random.py
@@ -18,41 +18,38 @@ def send_random_media(bot, message, file_path, media_type):
 
         send_func = send_funcs.get(media_type)
         if send_func:
-            send_func(message.chat.id, **{media_type: url}, reply_to_message_id=message.message_id)
+            send_func(message.chat.id, url, reply_to_message_id=message.message_id)
         else:
             bot.reply_to(message, "Không xác định kiểu media!")
     except Exception as e:
         bot.reply_to(message, f"Lỗi: {e}")
 
-COMMANDS = []
 
-# Video
-COMMANDS += [
+# Danh sách lệnh và loại media
+COMMANDS = [
+    # Video
     ("anime", "bot/url/anime.txt", "video"),
     ("girl",  "bot/url/girl.txt",  "video"),
-]
 
-# Photo
-COMMANDS += [
+    # Photo
     ("imganime",  "bot/url/imganime.txt",  "photo"),
     ("butt",      "bot/url/butt.txt",      "photo"),
     ("cosplay",   "bot/url/cosplay.txt",   "photo"),
     ("pussy",     "bot/url/pussy.txt",     "photo"),
     ("nude",      "bot/url/nude.txt",      "photo"),
     ("girlsexy",  "bot/url/girlsexy.txt",  "photo"),
-]
 
-# Animation
-COMMANDS += [
+    # Animation
     ("squeeze", "bot/url/squeeze.txt", "animation"),
 ]
 
 
-def create_handler(path, mtype):
+def create_handler(bot, path, mtype):
     def handler(message):
         send_random_media(bot, message, path, mtype)
     return handler
 
+
 def register_random(bot):
     for command, path, media_type in COMMANDS:
-        bot.message_handler(commands=[command])(create_handler(path, media_type))
+        bot.message_handler(commands=[command])(create_handler(bot, path, media_type))
