2092ec5c4401ac68d626792c9207c993229c853d
Hoang Anh <doviethoanganh07@gmail.com>
2025-06-30 21:39:57 +0700

    Update spamsms.py

--- bot/spamsms.py
+++ bot/spamsms.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import threading
 
 ADMIN_ID = 6379209139
-GROUP_ID = [6379209139, 5457864537]
+GROUP_ID = [6379209139, -1002408191237, 5900948782, 7605936504, 7944440933]
 VIP_FILE = "vip.txt"
 
 last_sms_time = {}
