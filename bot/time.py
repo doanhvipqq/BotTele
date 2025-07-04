413cb4cac08d38c9755c8daf395cfeecb9360587
Hoang Anh <doviethoanganh07@gmail.com>
2025-07-04 20:34:46 +0700

    Update time.py

--- bot/time.py
+++ bot/time.py
@@ -3,28 +3,28 @@ import random
 from datetime import timedelta
 
 start_time = time.time()
-file_path = "bot/url/anime.txt"
+file_url_video = "bot/url/anime.txt"
 
 def get_uptime():
     return str(timedelta(seconds=int(time.time() - start_time)))
 
-def anime_video():
+def video():
     try:
-        with open(file_path, "r", encoding="utf-8") as file:
+        with open(file_url_video, "r", encoding="utf-8") as file:
             video_urls = [line.strip() for line in file if line.strip()]
         
         if not video_urls:
             return None
 
         return random.choice(video_urls)
-    except Exception as e:
+    except Exception:
         return None
 
 def register_time(bot):
     @bot.message_handler(commands=['time'])
     def send_time(message):
         uptime = get_uptime()
-        video_url = anime_video()
+        video_url = video()
 
         if not video_url:
             bot.reply_to(message, "Không thể lấy video anime.")
