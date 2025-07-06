a773dfd77ed2d3544be7dfee325308caac5b820e
Hoang Anh <doviethoanganh07@gmail.com>
2025-07-06 18:27:21 +0700

    Update r34.py

--- bot/r34.py
+++ bot/r34.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 def register_r34(bot):
 	@bot.message_handler(commands=['r34'])
 	def handle_r34(message):
-
 		url = "https://rule34.xxx/index.php?page=post&s=random"
 		headers = {
 			"Referer": url,
@@ -15,15 +14,23 @@ def register_r34(bot):
 			response = requests.get(url, headers=headers, timeout=10)
 			soup = BeautifulSoup(response.text, "html.parser")
 
-			img_tags = soup.find_all("img") # Tìm tất cả thẻ <img>
-			exclude_src = ["https://rule34.xxx/static/icame.png"] # Link ảnh bạn muốn loại trừ
+			# Tìm tất cả thẻ <img>
+			img_tags = soup.find_all("img")
+
+			# Link ảnh bạn muốn loại trừ
+			exclude_src = ["https://rule34.xxx/static/icame.png"]
 
 			# In ra các src của ảnh
 			for img in img_tags:
 				src = img.get("src", "")
 				if src in exclude_src:
 					continue
-				bot.send_photo(message.chat.id, src, reply_to_message_id=message.message_id,)
-				return
+
+			bot.send_photo(message.chat.id, src, reply_to_message_id=message.message_id)
+			return
+
+			# # Nếu không có ảnh phù hợp
+			# bot.reply_to(message, "❌ Không tìm thấy ảnh nào hợp lệ.")
+
 		except Exception:
-			bot.reply_to(message, "Đã xảy ra lỗi nội bộ. Admin đang trong quá trình sửa chữa.")
+			bot.reply_to(message, "❌ Đã xảy ra lỗi nội bộ. Admin đang trong quá trình sửa chữa.")
