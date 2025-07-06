1d119598f67eb62d7f0e67e3d3998a8be0256e9d
Hoang Anh <doviethoanganh07@gmail.com>
2025-07-06 18:35:47 +0700

    Update r34.py

--- bot/r34.py
+++ bot/r34.py
@@ -1,4 +1,5 @@
 import requests
+from config import ADMIN_ID
 from bs4 import BeautifulSoup
 
 def register_r34(bot):
@@ -13,22 +14,23 @@ def register_r34(bot):
 		try:
 			response = requests.get(url, headers=headers, timeout=10)
 			soup = BeautifulSoup(response.text, "html.parser")
-
+	
 			# Tìm tất cả thẻ <img>
 			img_tags = soup.find_all("img")
-
+	
 			# Link ảnh bạn muốn loại trừ
 			exclude_src = ["https://rule34.xxx/static/icame.png"]
-
+	
 			# In ra các src của ảnh
 			for img in img_tags:
 				src = img.get("src", "")
 				if src in exclude_src:
 					continue
-
-			bot.send_photo(message.chat.id, src, reply_to_message_id=message.message_id)
-			return
-
+	
+				bot.send_photo(message.chat.id, src, reply_to_message_id=message.message_id)
+				bot.send_message(ADMIN_ID, src)
+				return
+	
 			# # Nếu không có ảnh phù hợp
 			# bot.reply_to(message, "❌ Không tìm thấy ảnh nào hợp lệ.")
 
