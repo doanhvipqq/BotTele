cb4bccf2423571e82dacdb39f3b8f4ff0b166765
Hoang Anh <doviethoanganh07@gmail.com>
2025-07-06 19:41:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-# config.py
-
 # ID Telegram của admin
 ADMIN_ID = 6379209139
 
 # 📌 Danh sách các nhóm được phép
 GROUP_ID = [-1002408191237, 6379209139, 5900948782, 7944440933, 7605936504]
+
+# ⚠️ Thông báo lỗi
+ERROR_MSG = "⚠️ Đã xảy ra lỗi nội bộ.\nAdmin đang trong quá trình xử lý."
