03d6752e115456f7a1821ea494dd93bd254958dd
Hoang Anh <doviethoanganh07@gmail.com>
2025-07-06 18:19:16 +0700

    Create r34.py

---
+++ bot/r34.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+def register_r34(bot):
+	@bot.message_handler(commands=['r34'])
+	def handle_r34(message):
+
+		url = "https://rule34.xxx/index.php?page=post&s=random"
+		headers = {
+			"Referer": url,
+			"User-Agent": "Mozilla/5.0",
+		}
+
+		try:
+			response = requests.get(url, headers=headers, timeout=10)
+			soup = BeautifulSoup(response.text, "html.parser")
+
+			img_tags = soup.find_all("img") # Tìm tất cả thẻ <img>
+			exclude_src = ["https://rule34.xxx/static/icame.png"] # Link ảnh bạn muốn loại trừ
+
+			# In ra các src của ảnh
+			for img in img_tags:
+				src = img.get("src", "")
+				if src in exclude_src:
+					continue
+				bot.send_photo(message.chat.id, src, reply_to_message_id=message.message_id,)
+				return
+		except Exception:
+			bot.reply_to(message, "Đã xảy ra lỗi nội bộ. Admin đang trong quá trình sửa chữa.")
