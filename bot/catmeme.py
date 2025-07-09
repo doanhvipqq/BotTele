cc880228537501aa5ec11dd7ba61dd008d6212eb
Hoang Anh <doviethoanganh07@gmail.com>
2025-07-09 12:31:15 +0700

    Create catmeme.py

---
+++ bot/catmeme.py
@@ -0,0 +1,39 @@
+import random
+import requests
+from bs4 import BeautifulSoup
+from config import ADMIN_ID, ERROR_MSG
+
+def register_catmeme(bot):
+	@bot.message_handler(commands=['catmeme'])
+	def handle_catmeme(message):
+		page = random.randint(1, 200)
+		url = f"https://m.aigei.com/lib/gif/?page={page}&sub=oi_mao"
+		headers = {
+			"Referer": url,
+			"User-Agent": "Mozilla/5.0",
+		}
+
+		try:
+			response = requests.get(url, headers=headers, timeout=15)
+			soup = BeautifulSoup(response.text, "html.parser")
+
+			img_tags = soup.find_all("img", src=True)
+			img_urls = []
+
+			for tag in img_tags:
+				url = tag.get("src", "")
+				if url.startswith("//s1.aigei.com"):
+					full_url = "https:" + url
+					img_urls.append(full_url)
+
+			if img_urls:
+				random_url = random.choice(img_urls)
+				bot.send_animation(message.chat.id, random_url, reply_to_message_id=message.message_id)
+				return  # Dừng ngay khi gửi được ảnh đầu tiên
+
+			# Nếu không tìm thấy ảnh hợp lệ
+			bot.reply_to(message, ERROR_MSG)
+
+		except Exception as e:
+			bot.reply_to(message, ERROR_MSG)
+			bot.send_message(ADMIN_ID, f"⚠️ Lỗi khi xử lý lệnh /catmeme:\n{e}")
