4699b5e0fb209998ffdcce04a693dbf605922db4
Hoang Anh <doviethoanganh07@gmail.com>
2025-07-14 11:40:05 +0700

    Update scl.py

--- bot/scl.py
+++ bot/scl.py
@@ -9,7 +9,7 @@ from telebot import types
 API_BASE = "https://api-v2.soundcloud.com"
 CONFIG_PATH = "config.json"
 
-HEADERS = {"User-Agent": "Mozilla/5.0}
+HEADERS = {"User-Agent": "Mozilla/5.0"}
 
 scl_data = {}
 
