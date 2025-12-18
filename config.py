26b44aea396a32ff09aa1a620e286aad9909cb2e
Bóng X <doanh6577765@gmail.com>
2025-12-19 01:37:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 # config.py
 
-# ID admin (bắt buộc là int)
 ADMIN_ID = 7509896689
-
-# Danh sách group được phép dùng bot (bắt buộc là list)
 GROUP_ID = [-1002256706038]
+
+ERROR_MSG = "⚠️ Bạn không có quyền sử dụng lệnh này."
